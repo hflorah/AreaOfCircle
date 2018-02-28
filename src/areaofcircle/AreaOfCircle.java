9927980b6d7dfd6ddb8bc10fac67564dcd75d392
huangsiwen <huangsiwen@10.5.144.97>
2018-02-28 11:09:21 -0500

    This is my first commit

---
+++ src/areaofcircle/AreaOfCircle.java
@@ -0,0 +1,27 @@
+/*
+ * Bennie
+ * AreaOfCircle.java
+ * This program calculates the area of a Circle
+ * February 27,2018
+ */
+package areaofcircle;
+
+/**
+ * 
+ * @author huangsiwen
+ */
+public class AreaOfCircle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double r,pi,A;
+        r=15.0;
+        pi=3.14;
+        A=pi*r*r;
+        
+        System.out.println("the area of the circle is "+A+" cm^2");
+    }
+    
+}
